eb23e634e347b780fe7cbf9d9711f05c8c25c4fb
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-25 14:09:33 -0400

    Navbar and focus swap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import "./App.css";
 import importProjects from "./projects.json";
 import RenderAll from "./components/RenderAll";
 import RenderFeatured from "./components/RenderFeatured";
+import NavBar from "./components/NavBar/NavBar";
 
 class App extends Component {
   state = {
-    focus: "featured"
+    focus: "all",
+    sort: "default"
+  };
+  changeFocus = newFocus => {
+    this.setState({ focus: newFocus });
   };
   render() {
     return (
@@ -15,18 +20,23 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">How do you want to do this?</h1>
+          <br />
+          <NavBar focus={this.state.focus} changeFocus={this.changeFocus} />
         </header>
         <div className="container">
+          {/* render only featured projects */}
           {this.state.focus === "featured" ? (
-            <RenderFeatured array={importProjects} />
+            <RenderFeatured array={importProjects} sort={this.state.sort} />
           ) : (
             ""
           )}
+          {/* render all projects */}
           {this.state.focus === "all" ? (
-            <RenderAll array={importProjects} />
+            <RenderAll array={importProjects} sort={this.state.sort} />
           ) : (
             ""
           )}
+          {/* render about me/contact */}
         </div>
       </div>
     );
