18d64d525712ae56aa60714be81d8966ec73fb8d
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-26 13:06:48 -0400

    changing structure to class to use componentDidMount

--- src/components/RenderSingle/RenderSingle.js
+++ src/components/RenderSingle/RenderSingle.js
@@ -1,23 +1,36 @@
-import React from "react";
-const RenderSingle = props => (
-  <div className="card singleTextBox">
-    {props.callAPI(props.elements.gitHub.replace("https://github.com/", ""))}
+import React, { Component } from "react";
+class RenderSingle extends Component {
+  componentDidMount = () => {
+    this.props.callAPI(
+      this.props.elements.gitHub.replace("https://github.com/", "")
+    );
+  };
+  render() {
+    return (
+      <div className="card singleTextBox">
+        {/* {props.callAPI(props.elements.gitHub.replace("https://github.com/", ""))} */}
+        <div>
+          <img
+            className="img-fluid singleImage"
+            src={this.props.elements.thumbnail}
+            alt={this.props.elements.name}
+          />
+          <a href={this.props.elements.gitHub}>GitHub</a>{" "}
+          {this.props.elements.deployed ? (
+            <a href={this.props.elements.deployed}>Deployed</a>
+          ) : (
+            ""
+          )}
+          {this.props.elements.video ? (
+            <a href={this.props.elements.video}>Video</a>
+          ) : (
+            <p />
+          )}
+          <p>{this.props.elements.description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
-    <div>
-      <img
-        className="img-fluid singleImage"
-        src={props.elements.thumbnail}
-        alt={props.elements.name}
-      />
-      <a href={props.elements.gitHub}>GitHub</a>{" "}
-      {props.elements.deployed ? (
-        <a href={props.elements.deployed}>Deployed</a>
-      ) : (
-        ""
-      )}
-      {props.elements.video ? <a href={props.elements.video}>Video</a> : <p />}
-      <p>{props.elements.description}</p>
-    </div>
-  </div>
-);
 export default RenderSingle;
