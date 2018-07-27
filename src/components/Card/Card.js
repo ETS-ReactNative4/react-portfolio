ddcbbbf048d6909c57a010d4b72262005b4515a7
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-27 18:19:16 -0400

    Revert "Updates"
    This reverts commit 325333849706600920e61d6094e3faea8095822e.

---
+++ src/components/Card/Card.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Card = props => (
+  <div className="card projectThumbnail">
+    {/* {console.log(props.technologies)} */}
+    <img
+      className=" img-fluid"
+      src={props.thumbnail}
+      alt={props.name}
+      onClick={() => {
+        props.changeFocus(props);
+      }}
+    />
+    <div className="card-body">
+      <h1 className="card-text">{props.name}</h1>
+      <p>
+        <a href={props.gitHub}>GitHub</a>
+        {props.deployed || props.video ? " | " : ""}
+        {props.deployed ? <a href={props.deployed}>Deployed</a> : ""}
+        {props.video ? <a href={props.video}>Video</a> : ""}
+      </p>
+      <p>{props.technologies}</p>
+    </div>
+  </div>
+);
+
+export default Card;
---
+++ src/components/RenderSingle/RenderSingle.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+class RenderSingle extends Component {
+  componentDidMount = () => {
+    this.props.callAPI(
+      this.props.elements.gitHub.replace("https://github.com/", "")
+    );
+  };
+
+  render() {
+    return (
+      <div className="card singleTextBox">
+        <div>
+          <img
+            className="img-fluid singleImage"
+            src={this.props.elements.thumbnail}
+            alt={this.props.elements.name}
+          />
+          <div className="singleLink">
+            <a href={this.props.elements.gitHub}>GitHub</a>{" "}
+            {this.props.elements.deployed || this.props.elements.video
+              ? " | "
+              : ""}
+            {/* conditional for the deployed link */}
+            {this.props.elements.deployed ? (
+              <a href={this.props.elements.deployed}>Deployed</a>
+            ) : (
+              ""
+            )}
+            {/* conditional for the video link */}
+            {this.props.elements.video ? (
+              <a href={this.props.elements.video}>Video</a>
+            ) : (
+              <p />
+            )}
+          </div>
+          {/* the description */}
+          <p>{this.props.elements.description}</p>
+          {/* the api results */}
+          <div className="apiResults">
+            <p className="resultsHeader">Five most recent commits: </p>
+            {this.props.results.map(currVal => {
+              return (
+                <p key={currVal.commit.committer.date}>
+                  {currVal.commit.committer.date.split("T")[0]}:{" "}
+                  {currVal.commit.message}
+                </p>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RenderSingle;
