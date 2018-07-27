afaccc067c9efa2839a1dfc7a50fde490bfff561
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-27 09:29:33 -0400

    responsive navbar fix

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,11 +3,11 @@ const NavBar = props => (
   <div className="container">
     <div className="row">
       {props.focus === "featured" ? (
-        <h3 className="col-xs-12 col-sm-6 col-md-4 focus">Featured Projects</h3>
+        <h3 className="col-xs-12 col-md-4 focus">Featured Projects</h3>
       ) : (
         <h3
           onClick={() => props.changeFocus("featured")}
-          className="col-xs-12 col-sm-6 col-md-4 navPill "
+          className="col-xs-12 col-md-4 navPill "
         >
           Featured Projects
         </h3>
@@ -17,17 +17,17 @@ const NavBar = props => (
       ) : (
         <h3
           onClick={() => props.changeFocus("all")}
-          className="col-xs-12 col-sm-6 col-md-4 navPill"
+          className="col-xs-12 col-md-4 navPill"
         >
           All Projects
         </h3>
       )}{" "}
       {props.focus === "about" ? (
-        <h3 className="col-xs-12 col-sm-6 col-md-4 focus">About Me</h3>
+        <h3 className="col-xs-12 col-md-4 focus">About Me</h3>
       ) : (
         <h3
           onClick={() => props.changeFocus("about")}
-          className="col-xs-12 col-sm-6 col-md-4 navPill"
+          className="col-xs-12 col-md-4 navPill"
         >
           About Me
         </h3>
