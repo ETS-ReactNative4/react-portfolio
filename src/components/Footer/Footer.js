bf02dce9223c1b3347dedc116259e0c8d1c985b9
Marcus Hilaire <marcushilaire@gmail.com>
2018-07-29 16:42:48 -0400

    Updates

--- src/components/Footer/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer className="fixed-bottom">
-    <p id="footertext">Copyright © 2018 Marcus Hilaire</p>
-  </footer>
-);
-
-export default Footer;
