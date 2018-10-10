a34aaa5ac5c837acfaad4fb30a4733bae7ac0c59
Marcus Hilaire <marcushilaire@gmail.com>
2018-10-10 10:57:12 -0400

    Next Steps

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -31,7 +31,28 @@ const AboutMe = props => (
           blandit. Integer consectetur auctor dolor in ultrices. Nunc placerat
           maximus nisi vel faucibus. Quisque venenatis fringilla lacinia. Sed in
           ligula at quam mollis interdum. Phasellus efficitur tincidunt
-          elementum.
+          elementum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis dolor. Praesent lacinia lacus quis ipsum aliquam
+          laoreet. Sed tempor imperdiet sapien. Proin nec nisi placerat,
+          vulputate leo vel, fringilla tellus. Nam sollicitudin dolor a sodales
+          porttitor. Nam eu porta risus. Proin consequat pretium nibh, nec
+          euismod turpis varius nec. Nam a sapien rutrum lorem congue finibus a
+          non arcu. Suspendisse nec lorem vehicula, tincidunt mauris id, lacinia
+          tellus. Nulla fermentum mi bibendum, tincidunt mi eu, facilisis erat.
+          Suspendisse aliquet tincidunt lorem, nec eleifend neque faucibus vel.
+          Proin in suscipit mi, nec vestibulum sapien. Proin augue arcu,
+          vehicula id ultrices at, lobortis ut massa. Orci varius natoque
+          penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          Vestibulum quam ante, lobortis in velit a, pharetra laoreet velit. Sed
+          pellentesque suscipit ligula. Aliquam neque ante, fermentum et purus
+          at, blandit malesuada velit. Sed porta pulvinar turpis a consequat.
+          Sed id lorem eu dui fringilla ultricies ac sed ligula. Integer nulla
+          metus, sagittis at neque at, vulputate facilisis magna. Praesent
+          venenatis pretium est ac aliquet. Integer aliquam turpis vel mauris
+          tincidunt blandit. Integer consectetur auctor dolor in ultrices. Nunc
+          placerat maximus nisi vel faucibus. Quisque venenatis fringilla
+          lacinia. Sed in ligula at quam mollis interdum. Phasellus efficitur
+          tincidunt elementum.
         </p>
       </div>
     </div>
